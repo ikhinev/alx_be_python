3f9ab4b70c9bca8da6fd01a5cb578d35933e5756
ikhinev <ikhinevictor1234@gmail.com>
2025-07-27 13:38:58 +0100

    Add perform_operation function to arithmetic_operations.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -4,6 +4,6 @@ size = int(input("Enter the size of the pattern: "))
 row = 0
 while row < size:
     for column in range(size):
-        print("*", end="")
+        print(column, end="")
     print()  
     row += 1
